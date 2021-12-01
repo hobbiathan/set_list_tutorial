9ea4106e2e655decd4eea077d05b59be3ee5239d
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-01 16:27:09 -0700

    magic commit l o l

--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -1,19 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'the songs show page' do
-  # As a visitor,
-  # when I visit '/songs/1'
-  # then I see that song's title, and artist
-
 
   it 'displays the song title' do
-    artist = Artist.create(name: "Scuba Dvala")
-    song = Song.create(title: "Mumble", length: 331, play_count: 17)
+    artist = Artist.create!(name: "Scuba Dvala")
+    song = artist.songs.create!(title: "Mumble", length: 331, play_count: 17)
+    song_two = artist.songs.create!(title: "Runner", length: 351, play_count: 1)
 
     # localhost:3000/songs/1
     visit "/songs/#{song.id}"
+
+    expect(page).to have_content(song.title)
+    expect(page).to_not have_content(song_two.title)
   end
 
-  it 'displays the name of song artist'
+  it 'displays the name of song artist' do
+    artist = Artist.create!(name: "Scuba Dvala")
+    song = artist.songs.create!(title: "Mumble", length: 331, play_count: 17)
+    song_two = artist.songs.create!(title: "Runner", length: 351, play_count: 1)
+
+    # localhost:3000/songs/1
+    visit "/songs/#{song.id}"
+    save_and_open_page
+    expect(page).to have_content(artist.name)
+  end
 
 end
