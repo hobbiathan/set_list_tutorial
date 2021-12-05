6cbc2d0c1229d2ceeabc0fd3a7e445d038161170
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-05 14:17:21 -0700

    Create route /artists

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
+  get '/artists', to: 'artists#index'
   get '/artists/:artist_id/songs', to: 'artist_songs#index'
 
 end
