8661d1e24b526073452938e16b3c411ba70e9dec
Ruben Pasillas <hobbbes@protonmail.com>
2021-12-05 14:36:03 -0700

    Adds artists new and create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
   get '/artists', to: 'artists#index'
+  get '/artists/new', to: 'artists#new'
   get '/artists/:artist_id/songs', to: 'artist_songs#index'
+
+  post '/artists', to: 'artists#create'
 
 end
--- spec/features/artists/new_spec.rb
+++ spec/features/artists/new_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe 'The Artist Creation' do
     click_link("New Artist")
     expect(current_path).to eq("/artists/new")
   end
+
+  it 'can create a new artist' do
+    visit '/artists/new'
+
+    fill_in('Name', with: 'TOPS')
+    click_button('Create Artist')
+
+    new_artist_id = Artist.last.id
+
+    expect(current_path).to eq("/artists")
+    expect(page).to have_content("TOPS")
+  end
 end
